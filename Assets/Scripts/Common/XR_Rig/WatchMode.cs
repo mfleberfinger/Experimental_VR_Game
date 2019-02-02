e76c0e2dc5691f31f37815f49308c07af0b369e8
Matthew <mfleberfinger@gmail.com>
2019-02-02 16:24:39 -0500

    Removed unneeded things from the XR rig and added simple XR rig scene setup tests to MainMenu scene.

--- Assets/Scripts/Common/XR_Rig/WatchMode.cs
+++
@@ -1,1 +0,0 @@
-﻿public enum WatchMode {clock, manual}
---
+++ Assets/Tests/EditMode_Tests/MainMenu_Tests/XRRig_Scene_Tests.cs
@@ -0,0 +1,72 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SpatialTracking;
+
+namespace Tests
+{
+	/// <summary>
+	/// Test whether the XR Rig is in the scene and properly configured.
+	/// </summary>
+	public class XRRig_Scene_Tests
+	{
+		/// <summary>
+		/// SetUp method to open the proper scene for testing.
+		/// </summary>
+		[SetUp]
+		public void SetUp()
+		{
+			EditorSceneManager.OpenScene("Assets/Scenes/MainMenu.unity");
+		}
+        
+		/// <summary>
+		/// Test for the XR Rig's existence in the scene.
+		/// </summary>
+		[Test]
+		public void XR_Rig_Found_Test()
+		{
+			GameObject rig = null;
+			rig = GameObject.Find("XR Rig");
+			Assert.That(rig, Is.Not.Null, "XR Rig not in scene or not enabled.");
+		}
+
+		/// <summary>
+		/// Test for a camera on the XR Rig.
+		/// </summary>
+		[Test]
+		public void XR_Rig_Has_Camera_Test()
+		{
+			GameObject rig = null;
+			Camera cam = null;
+
+			// Assumptions
+			rig = GameObject.Find("XR Rig");
+			Assume.That(rig, Is.Not.Null, "XR Rig not in scene or not enabled.");
+
+			// Assertions
+			cam = rig.GetComponentInChildren<Camera>();
+			Assert.That(cam, Is.Not.Null, "Camera not found on XR Rig.");
+		}
+
+		/// <summary>
+		/// Make sure the XR Rig has a tracked pose driver for the camera and
+		/// each hand.
+		/// </summary>
+		[Test]
+		public void XR_Rig_Has_TrackedPoseDrivers()
+		{
+			GameObject rig = null;
+			TrackedPoseDriver[] trackedPoseDrivers = null;
+
+			// Assumptions
+			rig = GameObject.Find("XR Rig");
+			Assume.That(rig, Is.Not.Null, "XR Rig not in scene or not enabled.");
+
+			// Assertions
+			trackedPoseDrivers = rig.GetComponentsInChildren<TrackedPoseDriver>();
+			Assert.That(trackedPoseDrivers.Length, Is.EqualTo(3), "There should" +
+				" be exactly 3 tracked pose drivers on the XR Rig.");
+		}
+	}
+}
