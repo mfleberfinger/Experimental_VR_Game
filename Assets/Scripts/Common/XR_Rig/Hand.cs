28fee227cad012ccb043b9f5b020ff7f29ca74e9
Matthew <mfleberfinger@gmail.com>
2019-02-02 18:05:15 -0500

    Added capacitive touch sensor pointing. Rotation of hands and pointers needs to be figured out.

---
+++ Assets/Scripts/Common/XR_Rig/Hand.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+	[Tooltip("The unity \"Axis\" name for the capacitive touch sensor used to" +
+		" decide whether the player is pointing.")]
+	public string triggerTouchButtonName;
+	[Tooltip("Gameobject to visibly indicate where the player is pointing.")]
+	public GameObject pointer;
+
+	private void Start()
+	{
+		pointer.SetActive(false);
+	}
+
+	private void Update()
+	{
+		Point();
+	}
+
+	/// <summary>
+	/// Determine whether the player is pointing and display or hide pointer
+	/// visual.
+	/// </summary>
+	private void Point()
+	{
+		if (Input.GetButton(triggerTouchButtonName))
+			pointer.SetActive(false);
+		else
+			pointer.SetActive(true);
+	}
+}
