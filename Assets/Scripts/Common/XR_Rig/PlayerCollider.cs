091a55ac230d92e546aff5f7aec70628d0375212
Matthew <mfleberfinger@gmail.com>
2019-02-02 11:36:55 -0500

    Configured project for VR and attempted to make the XR Rig from the hackathon work in this project.

---
+++ Assets/Scripts/Common/XR_Rig/PlayerCollider.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Script to make a collider attached to the XR Rig size and position itself
+// such that the top of the collider is at the top of the player's head and the
+// bottom of the collider is at the player's feet.
+public class PlayerCollider : MonoBehaviour
+{
+	[Tooltip("The player/XR Rig's collider.")]
+	[SerializeField]
+	private Transform colliderTransform = null;
+	[Tooltip("The XR Rig's Floor Offset.")]
+	[SerializeField]
+	private Transform floor = null;
+	[Tooltip("The XR Rig's camera.")]
+	[SerializeField]
+	private Transform camTransform = null;
+
+	// For saving the current x and z scale of the collider.
+	private float m_xScale, m_zScale;
+
+	private void FixedUpdate()
+	{
+		// Place the collider's center halfway between the real-world floor
+		// and the player's headset.
+		float yPos = (camTransform.localPosition.y - floor.localPosition.y) * 0.5f;
+		colliderTransform.localPosition = new Vector3(camTransform.localPosition.x,
+			yPos, camTransform.localPosition.z);
+
+		// Scale the collider's height to be equal to the current distance
+		// between the floor and the headset unless that size is smaller than
+		// some minimum.
+		m_xScale = colliderTransform.localScale.x;
+		m_zScale = colliderTransform.localScale.z;
+		colliderTransform.localScale =
+			new Vector3(m_xScale,
+				camTransform.localPosition.y - floor.localPosition.y, m_zScale);
+		
+		if(colliderTransform.localScale.y < 0.1f)
+			colliderTransform.localScale = new Vector3(m_xScale, 0.1f, m_zScale);
+	}
+}
---
+++ Assets/Scripts/Common/XR_Rig/Watch.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+// Allows the watch display to show time or be controlled by other scripts.
+public class Watch : MonoBehaviour
+{
+	[Tooltip("How should the watch behave?")]
+	[SerializeField]
+	private WatchMode mode = WatchMode.clock;
+
+	[Tooltip("The UI Text object on the watch face.")]
+	[SerializeField]
+	private Text displayText = null;
+
+	private void Update()
+	{
+		if (mode == WatchMode.clock)
+			displayText.text = DateTime.Now.ToLocalTime().ToShortTimeString();
+	}
+
+	/// <summary>
+	/// Set the watch mode. It should be set to "Manual" to manual to set the
+	/// display directly from another script.
+	/// </summary>
+	/// <param name="mode">The WatchMode enum member describing the mode
+	/// to use.</param>
+	public void SetMode(WatchMode mode)
+	{
+		this.mode = mode;
+	}
+
+	/// <summary>
+	/// Set the text displayed on the watch.
+	/// </summary>
+	/// <param name="displayText">The text to display on the next Update() call.</param>
+	public void SetText(string displayText)
+	{
+		this.displayText.text = displayText;
+	}
+}
