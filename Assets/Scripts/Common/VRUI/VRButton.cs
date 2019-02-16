de328c6419e78102c85f18b46276147259da9ac9
Matthew <mfleberfinger@gmail.com>
2019-02-16 18:43:35 -0500

    Made the debug display hidden by default. Added some experimental UI stuff.

---
+++ Assets/Scripts/Common/VRUI/VRButton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class VRButton : MonoBehaviour
+{
+	[Tooltip("Functions to call when the button is pressed.")]
+	public UnityEvent callbacks;
+	[Tooltip("Trigger used to make this button interactable.")]
+	public Collider trigger;
+	[Tooltip("Color to use when the button is not being interacted with.")]
+	public Color defaultColor;
+	[Tooltip("Color to use when the button is hovered over.")]
+	public Color hoveredColor;
+	[Tooltip("The image used to represent the button.")]
+	public Image image;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        
+    }
+}
