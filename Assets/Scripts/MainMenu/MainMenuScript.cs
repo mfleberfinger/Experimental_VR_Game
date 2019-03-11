a47ff5c96c7e233433fe2af93a73c17bcd8dfbae
Matthew <mfleberfinger@gmail.com>
2019-03-11 19:37:18 -0400

    Pressing the play button now changes to the empty Game scene. Skeleton ScoreKeeper script added.

---
+++ Assets/Scripts/MainMenu/MainMenuScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Controls the UI 
+/// </summary>
+public class MainMenuScript : MonoBehaviour
+{
+	[Tooltip("Path to the scene in which the actual gameplay occurs.")]
+	public string scenePath;
+
+	/// <summary>
+	/// Start the game.
+	/// </summary>
+	public void Play()
+	{
+		SceneManager.LoadScene(scenePath);
+	}
+}
