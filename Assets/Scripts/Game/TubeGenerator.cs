cadd164bf3e6b682e74e2c1e7c40b9343df3ddd2
Matthew <mfleberfinger@gmail.com>
2019-03-31 14:40:18 -0400

    Added a function to find points on a circle. Added functions to rotate and translate point clouds.

--- Assets/Scripts/Game/TubeGenerator.cs
+++ Assets/Scripts/Game/TubeGenerator.cs
@@ -16,23 +16,48 @@ public class TubeGenerator : MonoBehaviour
 		" segment.")]
 	public int ringVertexCount = 12;
 	[Tooltip("Radius of the tube.")]
-	public float radius;
+	public float radius = 1f;
+
+	// Array of points on a circle centered at the origin. This will be translated
+	//	and rotated to decide where to generate vertices.
+	private Vector3[] m_originalCircle;
 
 	// Starting position of the next segment to be generated.
 	private Vector3 m_startOfSegment;
 	private Queue<GameObject> m_segments;
 
-	// Arrays to define a ring of connected vertices to be copied as the ends of
-	// each segment.
-	private Vector3[] m_ringVertices;
-	private Vector3[] m_ringNormals;
-	private Vector2[] m_ringUVs;
-	
+	//TODO: Delete test code.
+	Vector3 lastPlacement;
+
 	private void Start()
 	{
 		m_startOfSegment = transform.position;
 		m_segments = new Queue<GameObject>();
+		m_originalCircle = DefineCircle(ringVertexCount, radius);
+		//TODO: Delete test code.
+		lastPlacement = transform.position;
 	}
+
+	private void Update()
+	{
+		//TODO: Delete test code.
+		//---------------Test Code----------------
+		if (Vector3.Distance(lastPlacement, transform.position) > 0.01f)
+		{
+			GameObject s;
+			Vector3[] vees = RotatePoints(m_originalCircle, Vector3.forward, transform.forward);
+			vees = TranslatePoints(vees, Vector3.zero, transform.position);
+			foreach(Vector3 v in vees)
+			{
+				s = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+				s.transform.localScale = s.transform.localScale * 0.01f;
+				s.transform.position = v;
+			}
+			lastPlacement = transform.position;
+		}
+		//---------------Test Code----------------
+	}
+
 
 	/// <summary>
 	/// Create a ring of vertices in the given plane, connected by edges with
@@ -46,14 +71,73 @@ public class TubeGenerator : MonoBehaviour
 	/// <param name="vertices">Array to store the calculated vertex positions.</param>
 	/// <param name="normals">Array to store the calculated normals.</param>
 	/// <param name="uv">Array to store the calculated UVs.</param>
-	private void DefineRing(int numberOfVertices, float radius, Vector3 center,
+	private void CreateVertexRing(int numberOfVertices, float radius, Vector3 center,
 		Vector3 plane, out Vector3[] vertices, out Vector3[] normals, out Vector2[] uv)
 	{
 		vertices = new Vector3[numberOfVertices];
 		normals = new Vector3[numberOfVertices];
 		uv = new Vector2[numberOfVertices];
+	}
 
-		// Divide 360 degrees/2pi radians by numberOfVertices to get angle
-		// between vertices.
+	/// <summary>
+	/// Translate a cloud of points to a new position in 3D space.
+	/// </summary>
+	/// <param name="points">The current positions of all points to translate.</param>
+	/// <param name="oldPosition">The orginal center position of the points.</param>
+	/// <param name="newPosition">The new center position for the points.</param>
+	/// <returns>The translated points.</returns>
+	public static Vector3[] TranslatePoints(Vector3[] points, Vector3 oldPosition,
+		Vector3 newPosition)
+	{
+		Vector3[] translatedPoints = new Vector3[points.Length];
+		Vector3 translationVector = newPosition - oldPosition;
+
+		for (int i = 0; i < points.Length; i++)
+			translatedPoints[i] = points[i] + translationVector;
+
+		return translatedPoints;
+	}
+
+	public static Vector3[] RotatePoints(Vector3[] points, Vector3 oldDirection,
+		Vector3 newDirection)
+	{
+		Vector3[] rotatedPoints = new Vector3[points.Length];
+		Quaternion rotation = new Quaternion();
+
+		rotation.SetFromToRotation(oldDirection, newDirection);
+		for (int i = 0; i < points.Length; i++)
+			rotatedPoints[i] = rotation * points[i];
+
+		return rotatedPoints;
+	}
+
+	/// <summary>
+	/// Generate an array of points, evenly spaced around a circle centered on
+	/// the origin in the x-y plane.
+	/// </summary>
+	/// <param name="numPoints">Number of points to generate.</param>
+	/// <param name="radius">Radius of the circle.</param>
+	/// <returns>An array of points on the defined circle.</returns>
+	public static Vector3[] DefineCircle(int numPoints, float radius)
+	{
+		// Angle between any two of the points.
+		float angularSpacing = 0;
+		float x = 0, y = 0, theta = 0;
+		Vector3[] circle = new Vector3[numPoints];
+
+		if(numPoints < 1 || radius <= 0)
+			Debug.LogError("A circle must have a positive radius and more than" +
+				" one point.");
+
+		angularSpacing = 2 * Mathf.PI / numPoints;
+		for (int i = 0; i < numPoints; i++)
+		{
+			theta = i * angularSpacing;
+			x = radius * Mathf.Cos(theta);
+			y = radius * Mathf.Sin(theta);
+			circle[i] = new Vector3(x, y, 0);
+		}
+
+		return circle;
 	}
 }
---
+++ Assets/Tests/EditMode_Tests/Game_Tests/TubeGenerator_Tests.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{	
+	public class TubeGenerator_Tests
+	{
+		/// <summary>
+		/// Call DefineCircle and verify that all returned points are at the
+		/// correct distance from the origin and evenly spaced. Verify that
+		/// the correct number of points is returned. Verify that there are no
+		/// duplicate points.
+		/// </summary>
+		[Test]
+		public void DefineCircle_Test()
+		{
+			Vector3[] circle;
+			HashSet<Vector3> existingPoints;
+			int nextK;
+			float radius, expectDistance, distance;
+
+			// For several numbers of points...
+			for (int i = 2; i < 5; i++)
+			{
+				// For several radii...
+				for (int j = 1; j <= 5; j++)
+				{
+					// Create a circle.
+					circle = TubeGenerator.DefineCircle(i, j);
+					Assert.That(circle.Length, Is.EqualTo(i), "Wrong number of " +
+						"points returned.");
+					existingPoints = new HashSet<Vector3>();
+					expectDistance = Vector3.Distance(circle[0], circle[1]);
+					// For each point...
+					for (int k = 0; k < circle.Length; k++)
+					{
+						// Verify that the point is at the correct radius.
+						radius = Vector3.Distance(circle[k], Vector3.zero);
+						Assert.That(Mathf.Approximately(radius, j), Is.True,
+							"Point found at wrong radius.");
+
+						// Verify that no other point exists at this position.
+						Assert.That(existingPoints.Contains(circle[k]), Is.False,
+							"Overlapping points found.");
+						existingPoints.Add(circle[k]);
+
+						// Verify that the point is the same distance from its
+						//	neighbor as all of the other points.
+						nextK = k + 1 < circle.Length ? k + 1 : 0;
+						distance = Vector3.Distance(circle[k], circle[nextK]);
+						Assert.That(Mathf.Approximately(expectDistance, distance),
+							Is.True, "Inconsistent point spacing found.");
+					}
+				}
+			}
+		}
+	}
+}
