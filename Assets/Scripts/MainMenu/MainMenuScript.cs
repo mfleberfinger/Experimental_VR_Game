ec26d2de628ebfad4b0dddb2362ae77c7a974cdb
Nyarmith <powah.serge@gmail.com>
2019-03-31 13:31:11 -0400

    UI fiddling

--- Assets/Scripts/MainMenu/MainMenuScript.cs
+++ Assets/Scripts/MainMenu/MainMenuScript.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 /// <summary>
 /// Controls the UI 
@@ -19,11 +21,30 @@ public class MainMenuScript : MonoBehaviour
 		SceneManager.LoadScene(scenePath);
 	}
 
-	/// <summary>
-	/// Toggles the main-menu right-panel between score and achievements
-	/// </summary>
+    private string m_swapStr = "😂\n😂😂\n😂😂😂\nYEETle";
+    private string m_buttonSwap = "S'COREs";
+    private string m_titleSwap = "Achievements";
 	public void toggleScore(GameObject scoreMenu)
 	{
-		//SceneManager.LoadScene(scenePath);
+        var textobj = scoreMenu.GetComponentInChildren<TextMeshPro>();
+        string tmp = textobj.text;
+        textobj.text = m_swapStr;
+        m_swapStr = tmp;
+
+        var texts = scoreMenu.GetComponentsInChildren<Text>();
+        Text titleText = texts[0];
+        tmp = titleText.text;
+        titleText.text = m_titleSwap;
+        m_titleSwap = tmp;
+
+        Text btnText = texts[1];
+        tmp = btnText.text;
+        btnText.text = m_buttonSwap;
+        m_buttonSwap = tmp;
 	}
+
+    public void toggleButtonText(GameObject buttonText)
+    {
+        var text = buttonText.GetComponent<Text>();
+    }
 }
