5506ded2035c569f831c1473758e0e5a215c8dfe
Nyarmith <powah.serge@gmail.com>
2019-04-07 14:15:58 -0400

    ui experiment, new assets

---
+++ Assets/Scripts/Serge_Experiments/experimentUIcolorChangeThing.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class experimentUIcolorChangeThing : MonoBehaviour
+{
+
+    public static bool toggle = true;
+    public void callBackButton(GameObject caller)
+    {
+        var img = caller.GetComponent<Image>();
+        if (toggle)
+            img.color = Color.cyan;
+        else
+            img.color = Color.green;
+
+        toggle = !toggle;
+    }
+}
