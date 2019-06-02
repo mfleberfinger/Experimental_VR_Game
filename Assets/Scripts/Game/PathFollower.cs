62133b13f3a67cc774994374bf850276ac231fe0
Matthew <mfleberfinger@gmail.com>
2019-06-02 16:55:05 -0400

    Added procedural path generation.

---
+++ Assets/Scripts/Game/PathFollower.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// A script that causes the gameobject to which it is attached to move forward
+/// and turn, following a gradually changing, procedurally generated path.
+/// </summary>
+public class PathFollower : MonoBehaviour
+{
+	[Tooltip("Speed at which the path follower moves.")]
+	public float speed = 10f;
+	[Tooltip("Random number generator seed for path generation.")]
+	public string seed = "seed";
+
+	private PathGenerator pathGenerator;
+
+	private void Start()
+	{
+		pathGenerator = new PathGenerator(seed);
+		
+		// The follower is assumed to start at 0.
+		transform.position = Vector3.zero;
+	}
+
+	
+	private void Update()
+	{
+		float z = transform.position.z + speed * Time.deltaTime;
+		Move(z);
+	}
+
+	/// <summary>
+	/// Move the game object to which this is attached. Make sure it faces in
+	/// the direction of travel.
+	/// </summary>
+	/// <param name="z">The z-coordinate to move to.</param>
+	private void Move(float z)
+	{
+		// TODO: If using the physics engine, this will need to change.
+		Vector3 nextPosition = pathGenerator.GetPoint(z);
+		
+		transform.LookAt(nextPosition);
+		transform.position = nextPosition;
+	}
+}
---
+++ Assets/Scripts/Game/PathGenerator.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class PathGenerator
+{
+	// Initial PerlinNoise coordinates for controlling the x position of the follower.
+	float m_xpx, m_ypx;
+	// Initial PerlinNoise coordinates for controlling the y position of the follower.
+	float m_xpy, m_ypy;
+	// Initial PerlinNoise() return values, used to ensure that the patch will
+	// always start at Unity's scene origin.
+	float initialXPerlin, initialYPerlin;
+
+	/// <summary>
+	/// Constructor. Takes a seed and initializes values needed for use with
+	/// the PerlinNoise() function.
+	/// </summary>
+	/// <param name="seed"></param>
+	public PathGenerator(string seed)
+	{
+		InitPathGenerator(seed);
+	}
+
+	/// <summary>
+	/// Seed the random number generator and get the starting coordinates on
+	/// the Perlin noise plane.
+	/// </summary>
+	/// <param name="seed">Seed value for procedural path generation.</param>
+	public void InitPathGenerator(string seed)
+	{
+		Random.InitState(seed.GetHashCode());
+		m_xpx = Random.value;
+		m_ypx = Random.value;
+		m_xpy = Random.value;
+		m_ypy = Random.value;
+		initialXPerlin = PerlinFunction(m_xpx, m_ypx);
+		initialYPerlin = PerlinFunction(m_xpy, m_ypy);
+	}
+
+	/// <summary>
+	/// Generates the x and y coordinates along the path for a given z coordinate.
+	/// </summary>
+	/// <param name="z">Some world space z-coordinate.</param>
+	/// <returns>The point along the procedurally generated path with the given
+	/// z-coordinate.</returns>
+	public Vector3 GetPoint(float z)
+	{
+		float x = PerlinFunction(z + m_xpx, m_ypx) - initialXPerlin;
+		float y = PerlinFunction(z + m_xpy, m_ypy) - initialYPerlin;
+		return new Vector3(x, y, z);
+	}
+
+	/// <summary>
+	/// Internal Perlin noise based function used to control various qualities
+	/// of the noise being generated and thereby control the path.
+	/// </summary>
+	/// <param name="z"></param>
+	private float PerlinFunction(float x, float y)
+	{
+		return 100f * Mathf.PerlinNoise(x, y);
+	}
+}
