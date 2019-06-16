25daf36e76a484be9aeb6a6383928f7fb44f87ae
Matthew <mfleberfinger@gmail.com>
2019-06-16 15:55:21 -0400

    Added code to limit the slope of the tube. This is to avoid making the player look straight up or straight down to avoid discomfort.

--- Assets/Scripts/Game/PathGenerator.cs
+++ Assets/Scripts/Game/PathGenerator.cs
@@ -47,10 +47,10 @@ public class PathGenerator
 
 	/// <summary>
 	/// Generates the x, y, and z coordinates along the path for a given value
-	/// of w. The "w" can be thought of as a fourth spatial dimension or as an
-	/// arbitrary parameter to the Perlin noise functions.
+	/// of w.
 	/// </summary>
-	/// <param name="z">Some world space z-coordinate.</param>
+	/// <param name="w">The "w" can be thought of as a fourth spatial dimension
+	/// or as an arbitrary parameter to the Perlin noise functions.</param>
 	/// <returns>The point along the procedurally generated path with the given
 	/// w-coordinate.</returns>
 	public Vector3 GetPoint(float w)
@@ -59,6 +59,63 @@ public class PathGenerator
 		float y = PerlinFunction(w + m_xpy, m_ypy) - initialYPerlin;
 		float z = PerlinFunction(w + m_xpz, m_ypz) - initialZPerlin;
 		return new Vector3(x, y, z);
+	}
+
+	/// <summary>
+	/// Calculate the next point along the path, as in GetPoint(), but limit the
+	/// maximum rate at which the vertical position changes.
+	/// </summary>
+	/// <param name="w">The "w" can be thought of as a fourth spatial dimension
+	/// or as an arbitrary parameter to the Perlin noise functions.</param>
+	/// <param name="lastPoint">The last point used by the thing following this
+	/// path.</param>
+	/// <param name="maxSlope">y = mx + b. Don't climb or descend more steeply
+	/// than this.</param>
+	/// <returns>The point along the procedurally generated path with the given
+	/// w-coordinate, with its y-component modified to limit the slope of climb
+	/// or descent based on the given previous position (lastPoint).</returns>
+	public Vector3 GetPointWithLimitedSlope(float w, Vector3 lastPoint, float maxSlope)
+	{
+		Vector3 nextPoint = GetPoint(w);
+		float newY = LimitSlope(lastPoint, nextPoint, maxSlope);
+		nextPoint = new Vector3(nextPoint.x, newY, nextPoint.z);
+
+		return nextPoint;
+	}
+
+	/// <summary>
+	/// Limit the maximum angle that the path will climb or descend at (maximum
+	/// (change in y) / (change in x-z plane)). This is intended to avoid making
+	/// the player twist their neck to look straight up or down for extended
+	/// periods of time. This function could be used similarly to limit the rate
+	/// at which position on the other two axes changes if desired.
+	/// </summary>
+	/// <param name="previous">Previous point.</param>
+	/// <param name="next">Next point.</param>
+	/// <param name="maxSlope">Maximum slope.</param>
+	/// <returns>If next.y results in a slope with magnitude less than or equal
+	/// to maxSlope, then return next.y. Otherwise, return a y value that would
+	/// result in the slope being equal in magnitude to maxSlope.</returns>
+	private float LimitSlope(Vector3 previous, Vector3 next, float maxSlope)
+	{
+		float newDeltaY = 0f;
+		float newY = next.y;
+		Vector2 previousXZ = new Vector2(previous.x, previous.z);
+		Vector2 nextXZ = new Vector2(next.x, next.z);
+		float horizontalDistance = Vector2.Distance(previousXZ, nextXZ);
+		float verticalDisplacement = next.y - previous.y;
+
+		// If slope would be greater than the maximum, recalculate next.y such
+		// that the slope will equal maxSlope.
+		// (slope) = deltaY/deltaX => deltaY = (slope)(deltaX)
+		if (Mathf.Abs(verticalDisplacement / horizontalDistance) > maxSlope)
+		{
+			newDeltaY =
+				maxSlope * horizontalDistance * Mathf.Sign(verticalDisplacement);
+			newY = previous.y + newDeltaY;
+		}
+
+		return newY;
 	}
 
 	/// <summary>
